2e2c5337bf176b1de763828f18d9e784588e30e0
dmerentitis <dmerentitis@motivian.com>
2022-06-05 12:33:19 +0300

    Test

--- src/components/portfolio/Portfolio.jsx
+++ src/components/portfolio/Portfolio.jsx
@@ -1,9 +1,6 @@
 import React from 'react'
 import './portfolio.css'
 import IMG1 from '../../assets/portfolio1.png'
-import IMG2 from '../../assets/portfolio2.png'
-import IMG3 from '../../assets/portfolio3.png'
-import IMG4 from '../../assets/portfolio4.png'
 
 const Portfolio = () => {
   return (
