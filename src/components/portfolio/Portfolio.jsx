be527cb2c5295c2ffcab590dfe3c0cad36f12c34
dmerentitis <dmerentitis@motivian.com>
2022-06-06 17:19:23 +0300

    newprojects

--- src/components/portfolio/Portfolio.jsx
+++ src/components/portfolio/Portfolio.jsx
@@ -8,64 +8,44 @@ const Portfolio = () => {
       <h5>My Recent Work</h5>
       <h2>Portfolio</h2>
       <div className="container portfolio__container">
-        <article className='portfolio__item'>
+      <article className='portfolio__item'>
           <div className="portfolio__item-image">
           <img src={IMG2} alt="img1" />
           </div>
-          <h3>This is a portfolio item tittle</h3>
+          <h3>Current Portfolio</h3>
           <div className="portfolio__item-cta">
-          <a href="https://github.com/mere1313" className='btn'>Github</a>
-          <a href="" className='btn btn-primary' target='_blank'>Live Demo</a>
+          <a href="https://github.com/mere1313/Portfolio" className='btn'>Github</a>
+          <a href="home" className='btn btn-primary' target='_blank'>Live Demo</a>
           </div>
         </article>
         <article className='portfolio__item'>
           <div className="portfolio__item-image">
           <img src={IMG2} alt="img1" />
           </div>
-          <h3>This is a portfolio item tittle</h3>
+          <h3>Simple Portfolio</h3>
           <div className="portfolio__item-cta">
-          <a href="https://github.com/mere1313" className='btn'>Github</a>
-          <a href="" className='btn btn-primary' target='_blank'>Live Demo</a>
+          <a href="https://github.com/mere1313/Portfolio-Simple-Website" className='btn'>Github</a>
+          <a href="https://portofolio-simple.herokuapp.com/" className='btn btn-primary' target='_blank'>Live Demo</a>
           </div>
         </article>
         <article className='portfolio__item'>
           <div className="portfolio__item-image">
           <img src={IMG2} alt="img1" />
           </div>
-          <h3>This is a portfolio item tittle</h3>
+          <h3>Simple BirdGame</h3>
           <div className="portfolio__item-cta">
-          <a href="https://github.com/mere1313" className='btn'>Github</a>
-          <a href="" className='btn btn-primary' target='_blank'>Live Demo</a>
+          <a href="https://github.com/mere1313/BirdGame" className='btn'>Github</a>
+          <a href="https://birdgame-mered.herokuapp.com/" className='btn btn-primary' target='_blank'>Live Demo</a>
           </div>
         </article>
         <article className='portfolio__item'>
           <div className="portfolio__item-image">
           <img src={IMG2} alt="img1" />
           </div>
-          <h3>This is a portfolio item tittle</h3>
+          <h3>Simple SaladBar</h3>
           <div className="portfolio__item-cta">
-          <a href="https://github.com/mere1313" className='btn'>Github</a>
-          <a href="" className='btn btn-primary' target='_blank'>Live Demo</a>
-          </div>
-        </article>
-        <article className='portfolio__item'>
-          <div className="portfolio__item-image">
-          <img src={IMG2} alt="img1" />
-          </div>
-          <h3>This is a portfolio item tittle</h3>
-          <div className="portfolio__item-cta">
-          <a href="https://github.com/mere1313" className='btn'>Github</a>
-          <a href="" className='btn btn-primary' target='_blank'>Live Demo</a>
-          </div>
-        </article>
-        <article className='portfolio__item'>
-          <div className="portfolio__item-image">
-          <img src={IMG2} alt="img1" />
-          </div>
-          <h3>This is a portfolio item tittle</h3>
-          <div className="portfolio__item-cta">
-          <a href="https://github.com/mere1313" className='btn'>Github</a>
-          <a href="" className='btn btn-primary' target='_blank'>Live Demo</a>
+          <a href="https://github.com/mere1313/SaladBar-MereD" className='btn'>Github</a>
+          <a href="https://salad-bar-mered.herokuapp.com/" className='btn btn-primary' target='_blank'>Live Demo</a>
           </div>
         </article>
       </div>
